81a10a90e1f7ba4610e184ca6db3f7263ec1e702
JonLevine <jlevine@students.hackleyschool.org>
2014-07-24 15:05:43 -0400

    First Commit

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ use Rack::Flash, :sweep=>true
 
 def current_user
 	if session[:user_id]
-		@current_user=User.find(session[:user_id])
+		if @current_user=User.find(session[:user_id]).blank? then redirect '/sign-in' else @current_user=User.find(session[:user_id]) end
 	else
 		redirect '/sign-in'
 	end
